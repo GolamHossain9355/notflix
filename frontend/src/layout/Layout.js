c794eadacfe6463c71c9d1ee001bff298d3957df
michaelslaton <michaelaaronphoto@gmail.com>
2022-07-24 19:48:38 -0700

    Established initial layout(Navbar, css, folder structure, errors). Installed & implemented react router.

---
+++ frontend/src/layout/Layout.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Layout.css";
+import Navbar from "./navbar/Navbar";
+
+export default function Layout(){
+
+  return (
+    <div>
+      <Navbar/>
+      All the things.
+    </div>
+  );
+}

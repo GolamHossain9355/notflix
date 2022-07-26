537d23968b03aa01a7859510778df26e89384f77
michaelslaton <michaelaaronphoto@gmail.com>
2022-07-25 20:33:01 -0700

    Created Navbar functionality

--- frontend/src/layout/Layout.js
+++ frontend/src/layout/Layout.js
@@ -1,12 +1,10 @@
 import React from "react";
 import "./Layout.css";
-import Navbar from "./navbar/Navbar";
 
 export default function Layout(){
 
   return (
-    <div>
-      <Navbar/>
+    <div className="block">
       All the things.
     </div>
   );
---
+++ frontend/src/layout/navbar/navdata.js
@@ -0,0 +1,19 @@
+const navdata = [
+  {
+    id: 1,
+    name: "Action & Adventure",
+    url: "",
+  },
+  {
+    id: 2,
+    name: "Drama",
+    url: "",
+  },
+  {
+    id: 3,
+    name: "Comedy",
+    url: "",
+  },
+]
+
+export default navdata;

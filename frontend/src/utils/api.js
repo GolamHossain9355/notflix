2539805979b5872c70cb0265dedabe2fc679f170
Golam Hossain <rabby29hossain@gmail.com>
2022-07-31 15:52:25 -0400

    added api's file and the 8 apis were going to be using for this project

---
+++ frontend/src/utils/api.js
@@ -0,0 +1,21 @@
+//Movies:
+const TOP_250_MOVIES_API = process.env.REACT_APP_TOP_250_MOVIES_API;
+const MOST_POPULAR_100_MOVIES_API =
+  process.env.REACT_APP_MOST_POPULAR_100_MOVIES_API;
+
+//Tvs:
+const TOP_250_TVS_API = process.env.REACT_APP_TOP_250_TVS_API;
+const MOST_POPULAR_100_TVS_API = process.env.REACT_APP_MOST_POPULAR_100_TVS_API;
+
+/**
+ *TODO: Embed Movie or Tv NAME as strings at the end of api:
+ */
+const MOVIES_SEARCH_API = process.env.REACT_APP_MOVIES_SEARCH_API;
+const TV_SEARCH_API = process.env.REACT_APP_MOVIES_SEARCH_API;
+
+/**
+ *TODO: Embed Movie or Tv ID as strings at the end of api:
+ */
+const MOVIES_AND_TV_INFORMATION_API =
+  process.env.REACT_APP_MOVIES_AND_TV_INFORMATION_API;
+const YOUTUBE_TRAILERS_API = process.env.REACT_APP_YOUTUBE_TRAILERS_API;

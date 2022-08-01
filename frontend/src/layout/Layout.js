bd69fcc7135ef7b3fc25cd1bdd31ac0059dadfdb
michaelslaton <michaelaaronphoto@gmail.com>
2022-07-31 19:11:53 -0700

    Refined Nav Collapse button, refined sliding animations, prepwork for mobile

--- frontend/src/layout/Layout.js
+++ frontend/src/layout/Layout.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Layout.css";
 
+import Header from "./header/Header"
+
 export default function Layout(){
 
   return (
     <div className="block">
-      All the things.
+      <Header/>
     </div>
   );
 }

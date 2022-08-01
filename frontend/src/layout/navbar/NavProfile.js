13aa0b3af4b46b010abe2d30175f3b52ac41af1a
michaelslaton <michaelaaronphoto@gmail.com>
2022-08-01 16:07:57 -0700

    Image Upload Test

--- frontend/src/layout/navbar/NavProfile.js
+++ frontend/src/layout/navbar/NavProfile.js
@@ -4,7 +4,7 @@ export default function NavProfile(){
 
   return (
     <div className="nav__profile--wrapper">
-      <img src={process.env.PUBLIC_URL + `/imgs/avatars/dummy.png`} className="nav__profile--img"/>
+      <img src={process.env.PUBLIC_URL + `/imgs/avatars/dummy2.png`} className="nav__profile--img"/>
     </div>
   )
 }

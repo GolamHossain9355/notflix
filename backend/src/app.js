83da9fcda9966f5626243dd05328f9efa11ae616
Golam Hossain <rabby29hossain@gmail.com>
2022-08-01 23:21:33 -0400

    added backend for calling all movies based on order

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,18 +1,27 @@
-const express = require("express");
-const app = express();
-const morgan = require("morgan");
-const notFoundHandler = require("./errors/notFoundHandler");
-const errorHandler = require("./errors/errorHandler");
- 
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
+
 const cors = require("cors");
- 
-app.use(express.json());
+const morgan = require("morgan");
+const express = require("express");
+
+const errorHandler = require("./errors/errorHandler");
+const notFoundHandler = require("./errors/notFoundHandler");
+const moviesRouter = require("../src/movies/movies.router");
+// const seriesRouter = require("../src/series/series.router");
+
+const app = express();
+
+app.use(cors());
 app.use(morgan("dev"));
-//app.use(cors())
- 
+app.use(express.json());
+
+app.use("/movies", moviesRouter);
+// app.use("/series", seriesRouter);
+
 //Not found handler
 app.use(notFoundHandler);
 //error handler
 app.use(errorHandler);
- 
+
 module.exports = app;
--- backend/src/db/fixtures/movieData.js
+++ backend/src/db/fixtures/movieData.js
@@ -1,25 +1,39 @@
 const fetch = require("node-fetch");
 
-const titleTypes = ["feature", "tv_series", "tv_specials", "shorts"];
-
 async function movieData() {
   try {
-    const response = titleTypes.map((type) => {
-      return fetch(
-        `https://imdb-api.com/API/AdvancedSearch/k_r87fjt2f?title_type=${type}&countries=us&languages=en&count=250&sort=boxoffice_gross_us,desc`
-      );
-    });
+    const response = [
+      fetch(
+        "https://imdb-api.com/API/AdvancedSearch/k_us23mp7b?title_type=feature&countries=us&languages=en&count=250&sort=boxoffice_gross_us,asc"
+      ),
+      fetch(
+        "https://imdb-api.com/API/AdvancedSearch/k_us23mp7b?title_type=feature&countries=us&languages=en&count=250&sort=boxoffice_gross_us,desc"
+      ),
+      fetch(
+        "https://imdb-api.com/API/AdvancedSearch/k_us23mp7b?title_type=feature&countries=us&languages=en&count=250&sort=user_rating,asc"
+      ),
+      fetch(
+        "https://imdb-api.com/API/AdvancedSearch/k_us23mp7b?title_type=feature&countries=us&languages=en&count=250&sort=user_rating,desc"
+      ),
+      fetch(
+        "https://imdb-api.com/API/AdvancedSearch/k_us23mp7b?title_type=feature&countries=us&languages=en&count=250"
+      ),
+      fetch(
+        "https://imdb-api.com/API/AdvancedSearch/k_us23mp7b?title_type=feature&countries=us&languages=en&count=250&sort=moviemeter,desc"
+      ),
+    ];
 
     const results = await Promise.all(response);
     const data = await Promise.all(results.map((item) => item.json()));
 
-    return data.reduce((allMovieTypesAdded, movieType) => {
+    return data.reduce((allMovieDataAdded, movieType) => {
       const reorganizedMovies = movieType.results.map((movie) => {
+        const date = movie.description.replace(/\D/g, "");
         return {
           movie_id: movie.id,
           image: movie.image,
           title: movie.title,
-          year_released: movie.description,
+          year_released: date,
           runtime: movie.runtimeStr,
           genres: movie.genres,
           content_rating: movie.contentRating,
@@ -29,8 +43,8 @@ async function movieData() {
           cast: movie.stars,
         };
       });
-      allMovieTypesAdded = allMovieTypesAdded.concat(reorganizedMovies);
-      return allMovieTypesAdded;
+      allMovieDataAdded = allMovieDataAdded.concat(reorganizedMovies);
+      return allMovieDataAdded;
     }, []);
   } catch (e) {
     console.error(e);

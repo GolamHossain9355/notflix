77bb5baadf9c02995be7c33a79e6a1fb38d64988
GolamHossain9355 <90365555+GolamHossain9355@users.noreply.github.com>
2022-09-05 16:25:33 -0400

    Delete .history/frontend directory

--- .history/frontend/src/contexts/AuthContext_20220905160201.js
+++
@@ -1,7 +0,0 @@
-import React, {useC} from 'react'
-
-export default function AuthContext() {
-  return (
-    <div>AuthContext</div>
-  )
-}
--- .history/frontend/src/contexts/AuthContext_20220905160230.js
+++
@@ -1,9 +0,0 @@
-import React, {useContext, useEffect, useState} from 'react'
-import {auth} from "../firebase"
-import { firebase } from 'firebase/compat/app';
-
-export default function AuthContext() {
-  return (
-    <div>AuthContext</div>
-  )
-}

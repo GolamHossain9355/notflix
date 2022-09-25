7dcdba58e5380516d5fe6315f54bdfda33eb5edb
Golam <rabby29hossain@gmail.com>
2022-09-25 00:03:42 -0400

    finished basic authentication functionality for sign in and sign up

---
+++ .history/frontend/src/utils/AuthenticationForm_20220924180435.js
@@ -0,0 +1,63 @@
+import React from "react";
+import "../layout/signIn/signIn.css";
+import {Header} from "../layout/header/Header";
+
+export default function AuthenticationForm({
+  error,
+  submitHandler,
+  emailRef,
+  passwordRef,
+  passwordConfirmRef,
+  loading,
+  title,
+}) {
+  return (
+    <>
+      <Header />
+      <div className="auth-container-outside">
+        <div className="auth-container-inside">
+          <h1>{title}</h1>
+          <h2>{error}</h2>
+          <form onSubmit={submitHandler}>
+            <div>
+              <label htmlFor="email">Email:</label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                ref={emailRef}
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="password">Password:</label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                ref={passwordRef}
+                required
+              />
+            </div>
+            {title === "Sign Up" && (
+              <div>
+                <label htmlFor="password-confirm">Confirm Password:</label>
+                <input
+                  type="password"
+                  name="password-confirm"
+                  id="password-confirm"
+                  ref={passwordConfirmRef}
+                  required
+                />
+              </div>
+            )}
+            <br />
+            <button type="submit" disabled={loading}>
+              {title}
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ frontend/src/layout/index.js
@@ -0,0 +1,8 @@
+export { default as GenrePage } from "./genrePage/GenrePage";
+export { default as MediaPage } from "./mediaPage/MediaPage";
+export { default as Navbar } from "./navbar/Navbar";
+export { default as SignIn } from "./signIn/SignIn";
+export { default as SignUp } from "./signUp/SignUp";
+export { default as ResetPassword } from "./resetPassword/ResetPassword";
+export { default as UpdateProfile } from "./updateProfile/UpdateProfile";
+export { default as PrivateRoutes } from "./PrivateRoutes";

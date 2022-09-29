7a0eedec7a2471c6a85282c623f406f0aabe7af6
michaelslaton <michaelaaronphoto@gmail.com>
2022-09-29 15:56:37 -0700

    Navbar Alteration

--- frontend/src/layout/navbar/components/NavProfile.js
+++ frontend/src/layout/navbar/components/NavProfile.js
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import dummy from "./dummy.png";
 
 export default function NavProfile() {
 
   return (
     <div className="nav__profile--wrapper">
-      <img src={dummy} className="nav__profile--img" />
+      <img src={dummy} alt="" className="nav__profile--img" />
     </div>
   );
 }

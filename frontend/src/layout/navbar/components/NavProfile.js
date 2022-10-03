e60264eb3673161e470e75624e1d7fce743e18c8
michaelslaton <michaelaaronphoto@gmail.com>
2022-10-02 17:09:43 -0700

    added user icons

--- frontend/src/layout/navbar/components/NavProfile.js
+++ frontend/src/layout/navbar/components/NavProfile.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { useAuth } from "../../../contexts/AuthContext";
-import dummy from "../../../assets/imgs/profileImages/dummy.png";
+import profileImages from "../../../data/profileImages";
 
 export default function NavProfile() {
   const { currentUser } = useAuth();
-
+  
   return (
     <div className="nav__profile--wrapper">
-      <img src={dummy} alt={currentUser?.displayName} className="nav__profile--img" />
+      <img src={profileImages[Number(currentUser.photoURL)].img} alt={currentUser?.displayName} className="nav__profile--img" />
       <div className="nav__profile--username">{currentUser?.displayName}</div>
     </div>
   );

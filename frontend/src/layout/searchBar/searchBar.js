ce8d5947b05fb10e9c27cf9f7d8bd6998c5c3b0c
Michael Slaton <69145809+michaelslaton@users.noreply.github.com>
2022-10-05 11:44:18 -0700

    Update searchBar.js

--- frontend/src/layout/searchBar/searchBar.js
+++ frontend/src/layout/searchBar/searchBar.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import "./searchBar.css";

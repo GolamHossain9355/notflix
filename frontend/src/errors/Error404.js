ba4c0484a73654f8c2937d75aecd0e647ea341a1
michaelslaton <michaelaaronphoto@gmail.com>
2022-10-21 11:12:19 -0700

    404 css refinements

--- frontend/src/errors/Error404.js
+++ frontend/src/errors/Error404.js
@@ -11,10 +11,10 @@ export default function Error404() {
           <div className="error__title">404</div>
           <div className="error__text">Not Found</div>
         </div>
-          <div className="error__links">
-            <button className="link" onClick={()=> navigate("/")}>Go Home ?</button>
-            <button className="link" onClick={()=> navigate(-1)}>Go Back ?</button>
-          </div>
+        <div className="error__links">
+          <button className="link" onClick={()=> navigate("/")}>Go Home ?</button>
+          <button className="link" onClick={()=> navigate(-1)}>Go Back ?</button>
+        </div>
       </div>
     </div>
   );
